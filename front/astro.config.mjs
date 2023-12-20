7789a7f1df7097c111f279ba0e210cd35c1acfc7
hidenari-pixel <hide8800kasu@gmail.com>
2023-12-20 22:19:38 +0900

    Install partytown

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown';
 
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
 });
